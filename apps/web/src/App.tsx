8a5d2d627fbfee4814ad3b763b10cc57bc84139f
ntwigs <oscar.cv.nordquist@gmail.com>
2023-03-14 19:54:38 +0100

    refactor: remove unused imports

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <p>Tjena</p>
+      <p>TURBOTRON</p>
     </div>
   )
 }
